91cbd86dee13f159f83a2584c1d4e3b1bb6f16bf
dacoev <justin.yim14@gmail.com>
2021-10-10 20:45:45 +0900

    인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function App() {
-  const name = undefined;
-  return <div>{name || '리액트'}</div>
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontweight: 'bold',
+    padding: '16px'
+  };
+  return <div style = {style}>{name}</div>
 }
 
 export default App;
