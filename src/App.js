b93d9700a7b06ba1f8207bf64ebfc58c142481e4
dacoev <justin.yim14@gmail.com>
2021-11-07 21:24:39 +0900

    props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import MyComponent from './MyComponent';
 import './App.css';
 
 const App = () =>{
-  return <MyComponent />;
+  return <MyComponent name="React"/>;
 }
+
+MyComponent.defaultProps = {
+  name: '기본 이름'
+};
 
 export default App;
